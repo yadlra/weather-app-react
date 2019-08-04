6eb75b2e2bd16124550542d8b31883a4bdee6648
yadlra <yadi.sb1@gmail.com>
2019-08-04 22:55:57 +0100

    Modify titles components

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,8 @@ class Titles extends React.Component {
   render() {
     return (
       <div>
-        <h1> Weather Now</h1>
-        <p> Find out weather conditions wherever you are...</p>
+        <h1 className="title-container__title">Weather Now</h1>
+        <h3 className="title-container__subtitle"> Find out weather conditions wherever you are...</h3>
       </div>
     );
   }
