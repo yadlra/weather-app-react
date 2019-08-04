242f1ccd5eee1bc85331a820520e02f392b00649
yadlra <yadi.sb1@gmail.com>
2019-08-04 20:33:59 +0100

    Create all component files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API_KEY ="f011724912ca210c9f553a2894282145";
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div>
+      <Titles />
+      <Form />
+      <Weather />
+      </div>
+    );
+  }
+};
 
 export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Titles extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Weather Now</h1>
+        <p> Find out weather conditions wherever you are...</p>
+      </div>
+    );
+  }
+};
+
+export default Titles;
