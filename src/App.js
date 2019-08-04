a27c06a0d9f00ae1a4876136334174f5e7d23f0c
yadlra <yadi.sb1@gmail.com>
2019-08-04 22:39:24 +0100

    Add bootstrap and css styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+
 const API_KEY ="f011724912ca210c9f553a2894282145";
 
 class App extends React.Component {
