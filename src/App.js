3ba43e8d127bbf4090bc604b331fd9a8e66ef662
yadlra <yadi.sb1@gmail.com>
2019-08-04 20:45:35 +0100

    Set API call and fetch url data"
    "

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import Weather from './components/Weather';
 const API_KEY ="f011724912ca210c9f553a2894282145";
 
 class App extends React.Component {
-  
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Southampton,uk&APPID=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    
+  }
   render() {
     return (
       <div>
