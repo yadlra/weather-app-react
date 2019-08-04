10a7863b98d53a4a312c14c51a3b1fbb9bc12166
yadlra <yadi.sb1@gmail.com>
2019-08-04 23:06:34 +0100

    Add value to weather props to have different appearance on app

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,22 +6,22 @@ class Weather extends React.Component {
     <div className="weather__info">
       {
         this.props.city && this.props.country && <p className="weather__key">Location:
-        <span>{ this.props.city}, { this.props.country}</span>
+        <span className="weather__value"> { this.props.city}, { this.props.country}</span>
         </p>
       }
       {
         this.props.temperature && <p className="weather__key">Temperature:
-        <span>{ this.props.temperature}</span>
+        <span className="weather__value"> { this.props.temperature}</span>
         </p>
       }
       {
         this.props.humidity && <p className="weather__key">Humidity:
-        <span>{ this.props.humidity}</span>
+        <span className="weather__value"> { this.props.humidity}</span>
         </p>
       }
       {
         this.props.description && <p className="weather__key">Conditions:
-        <span>{ this.props.description}</span>
+        <span className="weather__value"> { this.props.description}</span>
         </p>
       }
       {
