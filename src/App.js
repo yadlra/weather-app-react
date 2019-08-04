0b09f5efeb9865aa8eef34ecc7483562b795d64e
yadlra <yadi.sb1@gmail.com>
2019-08-04 21:52:14 +0100

    Make check if city and country values have been added, only then we want our code

--- src/App.js
+++ src/App.js
@@ -20,16 +20,19 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
-  }
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+     }
+    }
+
   render() {
     return (
       <div>
