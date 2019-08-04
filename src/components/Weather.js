0e66266c7fa569ff24ede3c6a13a63b17270efe6
yadlra <yadi.sb1@gmail.com>
2019-08-04 21:36:49 +0100

    Add description to state props

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,12 +4,11 @@ class Weather extends React.Component {
   render() {
     return (
     <div>
-      { this.props.city}
-      { this.props.country}
-      { this.props.temperature}
-      { this.props.humidity}
-      { this.props.description}
-      { this.props.error}
+      <p>Location: { this.props.city}, { this.props.country}</p>
+      <p>Temperature: { this.props.temperature}</p>
+      <p>Humidity: { this.props.humidity}</p>
+      <p>Conditions: { this.props.description}</p>
+      <p>{ this.props.error}</p>
     </div>
     );
   }
