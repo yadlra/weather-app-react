ef3dda6e13b73aac2ff2e357ecc56ebc6e4aa562
yadlra <yadi.sb1@gmail.com>
2019-08-04 21:11:05 +0100

    Initialize state and add description of state

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import Weather from './components/Weather';
 const API_KEY ="f011724912ca210c9f553a2894282145";
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
