0a0b38d3088f8ccacdaad79ac1cd2c100cf86789
yadlra <yadi.sb1@gmail.com>
2019-08-04 20:54:08 +0100

    Form component has access to the props in the form.js file

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import Weather from './components/Weather';
 const API_KEY ="f011724912ca210c9f553a2894282145";
 
 class App extends React.Component {
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Southampton,uk&APPID=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    
+    console.log(data);
   }
   render() {
     return (
       <div>
       <Titles />
-      <Form />
+      <Form getWeather={this.getWeather}/>
       <Weather />
       </div>
     );
