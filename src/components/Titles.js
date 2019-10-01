3f8be6e174298ad9368ffd3ae3c23e75b491efb8
yadlra <yadi.sb1@gmail.com>
2019-10-01 17:19:42 +0100

    Made class component into functional component

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class Titles extends React.Component {
-  render() {
-    return (
-      <div>
+
+
+// class Titles extends React.Component {
+//   render() { //this is the state? state > data we import to show user
+//     return (
+//       <div>
+//         <h1 className="title-container__title">Weather Now</h1>
+//         <h3 className="title-container__subtitle"> Find out weather conditions wherever you are...</h3>
+//       </div>
+//     );
+//   }
+// };
+
+// export default Titles;  THIS IS A STATEFUL = CLASS COMPONENT 
+
+const Titles = () => {
+  return (
+    <div>
         <h1 className="title-container__title">Weather Now</h1>
         <h3 className="title-container__subtitle"> Find out weather conditions wherever you are...</h3>
       </div>
-    );
-  }
-};
+  ) 
+}
 
-export default Titles;
+export default Titles; //THIS IS A STATELESS = FUNCTIONAL COMPONENT >> 
+// So HOOKS are this type of component but with state!! lol
+
+//> 1 State Hooks - possible to add state to functional components which are initially stateless
+//> 2 Effect Hooks - can add code to these which is then executed in response to component's lifecycle events
